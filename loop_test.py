f9d2a30155feb4eb6f1ad6906d97a20ef992f90c
dilux <diluxv@gmail.com>
2021-03-16 18:08:05 +0530

    array deviding

--- loop_test.py
+++ loop_test.py
@@ -1,4 +1,13 @@
-import os
-import sys
+from array import *
 
-print(sys.version)
+l1 = [1,2,3,4,5]
+l2 = [2,3,4,5,6]
+l3 = [3,4,5,6,7]
+
+l4 = [l1,l2,l3]
+
+print("col1 col2 col3 col4 col5")
+for i in l4:
+    for c in i:
+        print(" ",c, end="  ")
+    print()
