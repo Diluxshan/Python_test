0e1e9bb1ace91671556aaa1184eaafda45ee5836
dilux <diluxv@gmail.com>
2021-03-26 18:45:53 +0530

    text file to excel save

---
+++ save_excel.py
@@ -0,0 +1,20 @@
+import itertools
+
+import pandas as pd
+
+
+with open("your_text_data.txt") as data:
+    lines = [
+        [
+            i.split(":")[-1].replace("°C", "").replace("%RH", "")
+            for i in line.strip().split()
+            if i.startswith(("T1", "T2", "H1", "H2"))
+        ] for line in data.readlines() if line.strip()
+    ]
+    flat = list(itertools.chain.from_iterable(lines))
+    df = pd.DataFrame(
+        [flat[i:i+4] for i in range(0, len(flat), 4)],
+        columns=["T1", "H1", "T2", "H2"],
+    )
+    print(df)
+    df.to_excel("your_table.xlsx", index=False)
