ea158430c56a35a2dbb86a3ad4d213a0d5ccc5d0
dilux <diluxv@gmail.com>
2021-04-05 17:37:47 +0530

    new fiile for excel converting

--- save_excel.py
+++ save_excel.py
@@ -1,20 +1,15 @@
 #Sample code for Save the file in excel
-import itertools
+import re
+
 import pandas as pd
 
+with open("SHT_on_Stream.txt") as data_file:
+    data_list = re.findall(r"\d\d\.\d\d", data_file.read())
 
-with open(SHT_on_Stream.txt) as data:
-    lines = [
-        [
-            i.split(":")[-1].replace("°C", "").replace("%RH", "")
-            for i in line.strip().split()
-            if i.startswith(("T1", "T2", "H1", "H2"))
-        ] for line in data.readlines() if line.strip()
-    ]
-    flat = list(itertools.chain.from_iterable(lines))
-    df = pd.DataFrame(
-        [flat[i:i+4] for i in range(0, len(flat), 4)],
-        columns=["T1", "H1", "T2", "H2"],
-    )
-    print(df)
-    df.to_excel("your_table.xlsx", index=False)
+pd.DataFrame(
+    [data_list[i:i + 4] for i in range(0, len(data_list), 4)],
+    columns=["T1", "H1", "T2", "H2"],
+).to_excel(
+    "sht_on_stream.xlsx",
+    index=False,
+)
