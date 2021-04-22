800c7272a85a0e7045c9cb0792c4156dd2e805fa
dilux <diluxv@gmail.com>
2021-04-22 13:34:21 +0530

    delay of the pro

--- python_quee.py
+++ python_quee.py
@@ -1,4 +1,6 @@
 import queue
+import time
+import datetime
 
 q1 = queue.Queue(6)
 q1.put(1)
@@ -9,3 +11,8 @@ q1.put(5)
 print(q1.full())
 print(q1.qsize())
 print("Successfull finished")
+
+for i in range (0,10):
+    print(q1.qsize())
+    time.sleep(1)
+
